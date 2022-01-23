cb0e5721941d7f57eaf188e9b5f70b4f72c7fc09
dk460110 <dk460110@gmail.com>
2022-01-23 18:52:19 -0500

    static layouts updated, adding live updates

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 ## Dash and Plotly
 import dash
 from dash import html, dcc
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
 ## Helpers
 import queries
-#from config import pgs
+from config import pgs
 
 ## Standard
 from datetime import datetime
@@ -16,7 +17,7 @@ import pandas as pd
 import os
 
 # Get Data
-pgs = os.environ['pgs']
+#pgs = os.environ['pgs']
 engine = create_engine(pgs)
 
 ## Current Temp
@@ -24,9 +25,6 @@ cur_temp = pd.read_sql(queries.current_temp, con = engine, parse_dates = 'date')
 
 ## Earliest Data
 min_time = pd.read_sql(queries.min_date, con = engine, parse_dates = 'date')['min'][0].strftime('%B %d, %Y')
-
-## Daily Average
-df_daily = pd.read_sql(queries.daily, con = engine, parse_dates = 'date').sort_values('date')
 
 ## Record Low
 record_low = pd.read_sql(queries.low, con = engine)
@@ -40,21 +38,12 @@ df_hl = pd.read_sql(queries.daily_hl, con = engine)
 ## Last Week
 df_wk = pd.read_sql(queries.weekly, con = engine)
 
-## Daily Average Graph
-daily_fig = go.Figure()
-daily_fig.add_trace(go.Scatter(x = df_daily['date'], y = df_daily['temp'],
-                               line=dict(color='rgba(56,250,251,1)', width=2)))
-
-daily_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='white',
-                        showlegend = False, title_text = 'Average Daily Temperature',
-                        xaxis=dict(rangeslider=dict(visible = True), type = "date", showgrid = False),
-                        yaxis=dict(gridcolor = '#444444'),
-                        yaxis_title = "Temperature in °F")
-
 ## Daily Highs and Lows
 hl_fig = go.Figure()
 hl_fig.add_trace(go.Scatter(x = df_hl['min'], y = df_hl['max'], mode = "markers",
-                            marker=dict(color='rgba(56,250,251,1)')))
+                            marker=dict(color='rgba(56,250,251,1)'),
+                            text = [item.strftime('%b %d, %Y') for item in df_hl['date']],
+                            hovertemplate = '''Date: %{text}<br>Max: %{y:.2f}°F<br>Min: %{x:.2f}°F<extra></extra>'''))
 
 hl_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='white',
                         showlegend = False, title_text = 'Daily Highs and Lows',
@@ -66,7 +55,9 @@ hl_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)'
 ## Last Seven Days
 wk_fig = go.Figure()
 wk_fig.add_trace(go.Scatter(x = df_wk['date'], y = df_wk['temp'],
-                               line=dict(color='rgba(56,250,251,1)', width=2)))
+                               line=dict(color='rgba(56,250,251,1)', width=2),
+                               text = [item.strftime('%b %d, %Y %H:%M%p') for item in df_wk['date']],
+                               hovertemplate = '''Date and Time: %{text}<br>Temp: %{y:.2f}°F<extra></extra>'''))
 
 wk_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='white',
                         showlegend = False, title_text = 'Last Seven Days',
@@ -76,7 +67,8 @@ wk_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)'
 
 
 app = dash.Dash(external_stylesheets = [dbc.themes.DARKLY])
-server = app.server
+app.title = "Dustin's Temperature Dashboard"
+#server = app.server
 
 # Navbar
 navbar = dbc.NavbarSimple(
@@ -111,16 +103,19 @@ app.layout = html.Div([
                     dbc.Row(
                         dbc.Col([
                             html.H5(children="The Current Temperature at Dustin's House is " + str(cur_temp) + "°F"),
-                            html.P('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada proin libero nunc consequat interdum varius. Felis eget velit aliquet sagittis id consectetur. Elementum nibh tellus molestie nunc non blandit. Fringilla ut morbi tincidunt augue interdum. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Diam donec adipiscing tristique risus. Ac turpis egestas integer eget aliquet nibh. Sit amet est placerat in egestas.')]), style = {"margin-bottom": "2rem"}),
+                            html.P("Welcome to Dustin's Temperature Dashboard! Click the tabs below to view customized, interactive visualizations of the temperature outside " +
+                                   "of Dustin's house in Columbus, Ohio. This dashboard was created with Python, SQL, Git, and a few other languages. Links to the source code " +
+                                   "and data are available in the upper right-hand corner. The dashboard will refresh every 20 minutes to fetch the latest data.")]),
+                                   style = {"margin-bottom": "2rem"}),
                     dbc.Row(
                         dbc.Col(
                             dbc.Tabs([
-                                dbc.Tab(dcc.Graph(figure = daily_fig), label = 'Average Daily Temp'),
-                                dbc.Tab(dcc.Graph(figure = hl_fig), label = "Daily Highs and Lows"),
-                                dbc.Tab(dcc.Graph(figure = wk_fig), label = "Last Seven Days")]))),
+                                dbc.Tab(dcc.Graph(id = 'daily-figure'), label = 'Average Daily Temp'),
+                                dbc.Tab(dcc.Graph(figure = hl_fig, id = 'high-low-figure'), label = "Daily Highs and Lows"),
+                                dbc.Tab(dcc.Graph(figure = wk_fig, id = 'weekly-figure'), label = "Last Seven Days")]))),
                     dbc.Row(
                         dbc.Col([
-                            html.H3(children = "Records")], width = "auto"), justify = "center", style = {"margin-top": "5rem"}),
+                            html.H3(children = "Records")], width = "auto"), justify = "center", style = {"margin-top": "2rem"}),
                     dbc.Row(
                         dbc.Col([
                             html.H6(children="Collecting Data Since " + str(min_time), className="hello")], width = "auto"), justify = "center", style = {"margin-bottom": "2rem"}),
@@ -147,8 +142,31 @@ app.layout = html.Div([
                                                   title = "1 - Linux server requests temperature"),
                                 dbc.AccordionItem("The current temperature, once retrieved, is timestamped and written to a POSTGRESQL database on Bit.io",
                                                   title = "2 - Linux server stores temperature on Bit.io"),
-                                dbc.AccordionItem("Every 30 minutes Heroku refreshes this dashboard to display the latest data for all measurements and visualizations",
-                                                  title = "3 - Heroku queries and displays data")]), width = 8), justify = "center", style = {"margin-bottom": "20rem"})])])
+                                dbc.AccordionItem("Every 20 minutes Heroku refreshes this dashboard to display the latest data for all measurements and visualizations",
+                                                  title = "3 - Heroku queries and displays data")]), width = 8), justify = "center", style = {"margin-bottom": "20rem"})]),
+                dcc.Interval(id = 'interval-component', interval = 1200 * 1000, n_intervals = 0)])
+
+# Daily Average Figure
+@app.callback(Output('daily-figure', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_daily_averages(n):
+    # Collect data
+    df_daily = pd.read_sql(queries.daily, con = engine, parse_dates = 'date').sort_values('date')
+
+    # Build figure
+    daily_fig = go.Figure()
+    daily_fig.add_trace(go.Scatter(x = df_daily['date'], y = df_daily['temp'],
+                                   line=dict(color='rgba(56,250,251,1)', width=2),
+                                   text = [item.strftime('%b %d, %Y') for item in df_daily['date']],
+                                   hovertemplate = '''Date: %{text}<br>Avg: %{y:.2f}°F<extra></extra>'''))
+
+    daily_fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='white',
+                            showlegend = False, title_text = 'Average Daily Temperature',
+                            xaxis=dict(rangeslider=dict(visible = True), type = "date", showgrid = False),
+                            yaxis=dict(gridcolor = '#444444'),
+                            yaxis_title = "Temperature in °F")
+
+    return daily_fig
 
 
 if __name__ == '__main__':
